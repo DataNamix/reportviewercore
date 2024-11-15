33b85ca3520705fd5410cb76342e1b12d52600b3
Riccorbypro <riccorbypro@gmail.com>
2024-11-15 20:54:25 +0200

    Added linux docker image for testing

--- ReportViewerCore.Sample.AspNetCore/Reports/Report.cs
+++ ReportViewerCore.Sample.AspNetCore/Reports/Report.cs
@@ -13,7 +13,7 @@ namespace ReportViewerCore
 		{
 			var items = new[] { new ReportItem { Description = "Widget 6000", Price = widgetPrice, Qty = 1 }, new ReportItem { Description = "Gizmo MAX", Price = gizmoPrice, Qty = 25 } };
 			var parameters = new[] { new ReportParameter("Title", "Invoice 4/2020") };
-			using var rs = Assembly.GetExecutingAssembly().GetManifestResourceStream("ReportViewerCore.Sample.AspNetCore.Reports.Report.rdlc");
+			using var rs = Assembly.GetExecutingAssembly().GetManifestResourceStream("ReportViewerCore.Sample.AspNetCore.Reports.ImageReport.rdlc");
 			report.LoadReportDefinition(rs);
 			report.DataSources.Add(new ReportDataSource("Items", items));
 			report.SetParameters(parameters);
